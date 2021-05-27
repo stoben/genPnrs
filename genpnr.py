1f40c7000e85b1c12cd3bd88cd04e771b38baad2
Ben M Stokland <stoben@users.noreply.github.com>
2021-05-27 09:15:47 +0200

    init

---
+++ genpnr.py
@@ -0,0 +1,69 @@
+
+import sys
+import datetime
+
+
+def main(arg):
+
+    # if len(arg) < 2:
+    #     print("Invalid number of arguments")
+    #     return        
+    # else:
+    generateFile(datetime.datetime(1973,1,1), datetime.datetime(1974,1,1))
+
+
+def generateFile(start, stop:datetime):
+
+
+    
+    count = start
+    #personNumber = 1
+    while(count <= stop):         
+        for personnr in range(1000):
+            getPnr("{0:02d}{1:02d}{2:02d}{3:03d}".format(count.day, count.month, int(count.strftime("%y")), personnr))
+        count += datetime.timedelta(days=1)
+
+
+
+def getPnr(dtpnr):
+    sum = 0
+    multiplier = 2
+    constPre = [3,7,6,1,8,9,4,5,2,1]
+    constPost = [5,4,3,2,7,6,5,4,3,2,1]
+
+    num1 = 0
+    num2 = 0
+
+    c = 0
+    for i in dtpnr:        
+        if c < 9:
+            num1 += int(i) * constPre[c]         
+            num2 += int(i) * constPost[c]
+        c += 1
+    
+    #check control    
+    num1 = num1 % 11
+    if num1 != 0:
+        num1 = abs(11 - num1) #mod11
+
+    if num1 == 10: 
+        return None
+
+    
+    num2 += num1 * constPost[9]
+    
+    num2 = num2 % 11
+    if num2 != 0:
+        num2 = abs(11 - num2) #mod 11
+
+    if num2 == 10:
+        return None #not usable
+    
+
+    return print("{0}{1}{2}".format(dtpnr, num1, num2))
+    
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
